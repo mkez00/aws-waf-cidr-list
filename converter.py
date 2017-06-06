6776335f75dadd688142fd0d08f33e901838fc2d
Michael Kezele <michael.kezele@scenedoc.com>
2017-06-06 14:13:09 -0400

    code cleanup.  separate script for python3....not finished yet

--- converter.py
+++ converter.py
@@ -3,42 +3,128 @@ import requests
 import configparser
 import boto3
 
-print 'STARTING'
+def createUpdate(ipAddr):
 
-client = boto3.client('waf-regional')
-response = client.list_ip_sets()
-print response
+	update = {}
+	update['Action'] = 'INSERT'
+	update['IPSetDescriptor'] = createDescriptor('IPV4',ipAddr)
+	return update
+
+def createDescriptor(valueType,value):
+	descriptor = {}
+	descriptor['Type'] = valueType
+	descriptor['Value'] = value
+	return descriptor
+
+def getNewChangeTokenId(client):
+	changeToken = client.get_change_token()
+	return changeToken.get('ChangeToken')
+
+def updateIpSet(client,ipSetId,ipAddr):
+	changeTokenId = getNewChangeTokenId(client)
+	response = client.update_ip_set(
+    IPSetId=ipSetId,
+    ChangeToken=changeTokenId,
+    Updates=[
+		        {
+		            'Action': 'INSERT',
+		            'IPSetDescriptor': {
+		                'Type': 'IPV4',
+		                'Value': ipAddr
+		            }
+		        },
+    		]
+	)
+	print 'Updated IP set'
+	return
+
+def updateIpSetBulk(client,ipSetId,updates):
+	changeTokenId = getNewChangeTokenId(client)
+	response = client.update_ip_set(
+    IPSetId=ipSetId,
+    ChangeToken=changeTokenId,
+    Updates=updates
+	)
+	return
+
+def getConfigParser():
+	config = configparser.ConfigParser()
+	config.read("converter.conf")
+	return config
+
+def getBotoClient():
+	return boto3.client('waf-regional')
+
+def traverseListAndUpdateIpSetBulk(client,ipSetId):
+	count = 0
+	#get url and fetch content
+	url = config['DEFAULT']['DropListUrl']
+	r = requests.get(url)
+	lines = r.content.splitlines()
+
+	updatesList = []
+	for line in lines:
+		if line[0]!=';':
+			cidr = line.split(';')[0]
+			cidrSplit = cidr.split('/')
+			cidrRange = int(cidrSplit[1])
+
+			if cidrRange <= 8:
+				cidrRange = 8
+			elif cidrRange <= 16:
+				cidrRange = 16
+			elif cidrRange <= 24:
+				cidrRange = 24
+			else:
+				cidrRange = 32
+
+			net = IPNetwork(cidr)
+			subnets = net.subnet(cidrRange)
+			for subnet in subnets:
+				count += 1
+				updatesList.append(createUpdate(str(subnet)))
+				print subnet
+	updateIpSetBulk(client,ipSetId,updatesList[:1001])
+	return count
+
+def traverseListAndUpdateIpSet(client,ipSetId):
+	count = 0
+	#get url and fetch content
+	url = config['DEFAULT']['DropListUrl']
+	r = requests.get(url)
+	lines = r.content.splitlines()
+
+	for line in lines:
+		if line[0]!=';':
+			cidr = line.split(';')[0]
+			cidrSplit = cidr.split('/')
+			cidrRange = int(cidrSplit[1])
+
+			if cidrRange <= 8:
+				cidrRange = 8
+			elif cidrRange <= 16:
+				cidrRange = 16
+			elif cidrRange <= 24:
+				cidrRange = 24
+			else:
+				cidrRange = 32
+
+			net = IPNetwork(cidr)
+			subnets = net.subnet(cidrRange)
+			for subnet in subnets:
+				count += 1
+				updateIpSet(client,ipSetId,str(subnet))
+				print subnet
+	return count
+
+
+print 'STARTING!!!!'
+
+# configure stuff
+client = getBotoClient()
+config = getConfigParser()
+ipSetId = config['DEFAULT']['IpSetId']
 
 count = 0
-
-#config file
-config = configparser.ConfigParser()
-config.read("converter.conf")
-
-#get url and fetch content
-url = config['DEFAULT']['DropListUrl']
-r = requests.get(url)
-lines = r.content.splitlines()
-
-for line in lines:
-	if line[0]!=';':
-		cidr = line.split(';')[0]
-		cidrSplit = cidr.split('/')
-		cidrRange = int(cidrSplit[1])
-
-		if cidrRange <= 8:
-			cidrRange = 8
-		elif cidrRange <= 16:
-			cidrRange = 16
-		elif cidrRange <= 24:
-			cidrRange = 24
-		else:
-			cidrRange = 32
-
-		net = IPNetwork(cidr)
-		subnets = net.subnet(cidrRange)
-		#for subnet in subnets:
-		#	count += 1
-		#	print subnet
-
+count = traverseListAndUpdateIpSetBulk(client,ipSetId)
 print 'FINISHING TOTAL: ' + str(count) 
---
+++ converter3.py
@@ -0,0 +1,44 @@
+from netaddr import IPNetwork
+import requests
+import configparser
+import boto3
+
+print('STARTING')
+
+client = boto3.client('waf-regional')
+response = client.list_ip_sets()
+print(response)
+
+count = 0
+
+#config file
+config = configparser.ConfigParser()
+config.read("converter.conf")
+
+#get url and fetch content
+url = config['DEFAULT']['DropListUrl']
+r = requests.get(url)
+lines = r.content.splitlines()
+
+for line in lines:
+	if line[0]!=';':
+		cidr = line.split(';')[0]
+		cidrSplit = cidr.split('/')
+		cidrRange = int(cidrSplit[1])
+
+		if cidrRange <= 8:
+			cidrRange = 8
+		elif cidrRange <= 16:
+			cidrRange = 16
+		elif cidrRange <= 24:
+			cidrRange = 24
+		else:
+			cidrRange = 32
+
+		net = IPNetwork(cidr)
+		subnets = net.subnet(cidrRange)
+		for subnet in subnets:
+			count += 1
+			print(subnet)
+
+print('FINISHING TOTAL: ' + str(count))
